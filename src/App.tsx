8c0075cf4985627544a5df4741b057b8b69c6aae
sajjadabd <mynameissajjad@gmail.com>
2023-10-17 12:24:53 +0330

    Initial commit for react flow graph

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Flow from './Flow'
+
+function App() {
+
+
+  return (
+    <>
+      <div className="card">
+        <Flow />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Flow.tsx
@@ -0,0 +1,23 @@
+import ReactFlow from "reactflow";
+
+import "reactflow/dist/style.css";
+
+const initialNodes = [
+  { id: "1", position: { x: 200, y: 200 }, data: { label: "1" } },
+  { id: "2", position: { x: 200, y: 300 }, data: { label: "2" } },
+];
+
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+
+export default function Flow() {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <ReactFlow nodes={initialNodes} edges={initialEdges} />
+    </div>
+  );
+}
