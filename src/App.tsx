fd086fd54fe8e26701a3c92d43b8737e2b4c0605
sajjadabd <mynameissajjad@gmail.com>
2023-10-17 13:17:04 +0330

    Implement graph shapes with TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "./App.css";
 // import Flow from "./Flow";
-// import FlowInteractive from "./FlowInteractive";
+import FlowInteractive from "./FlowInteractive";
 import FlowInteractiveWithControl from "./FlowInteractiveWithControl";
+import FlowTypeScript from "./FlowTypeScript";
 
 function App() {
   return (
     <>
       <div className="card">
-        <FlowInteractiveWithControl />
+        <FlowTypeScript />
       </div>
     </>
   );
---
+++ src/FlowTypeScript.tsx
@@ -0,0 +1,70 @@
+import { useCallback } from "react";
+import ReactFlow, {
+  Node,
+  addEdge,
+  Background,
+  Edge,
+  Connection,
+  useNodesState,
+  useEdgesState,
+} from "reactflow";
+
+// import CustomNode from "./CustomNode";
+
+import "reactflow/dist/style.css";
+
+import CustomNode from "./CustomNode";
+
+const initialNodes: Node[] = [
+  {
+    id: "1",
+    type: "input",
+    data: { label: "Node 1" },
+    position: { x: 250, y: 5 },
+  },
+  { id: "2", data: { label: "Node 2" }, position: { x: 100, y: 100 } },
+  { id: "3", data: { label: "Node 3" }, position: { x: 400, y: 100 } },
+  {
+    id: "4",
+    type: "custom",
+    data: { label: "Node" },
+    position: { x: 400, y: 200 },
+  },
+];
+
+const initialEdges: Edge[] = [
+  { id: "e1-2", source: "1", target: "2", animated: true },
+  { id: "e1-3", source: "1", target: "3" },
+];
+
+const nodeTypes = {
+  custom: CustomNode,
+};
+
+const FlowTypeScript = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const onConnect = useCallback(
+    (params: Edge | Connection) =>
+      setEdges((els) => addEdge({ ...params, animated: true }, els)),
+    [setEdges]
+  );
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        nodeTypes={nodeTypes}
+        fitView
+      >
+        <Background gap={12} size={1} />
+      </ReactFlow>
+    </div>
+  );
+};
+
+export default FlowTypeScript;
